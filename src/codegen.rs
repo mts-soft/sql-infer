701a9a1619e1b719364d948a3f3cd0263503b746
jnlt3 <dsekercioglu2003@gmail.com>
2025-02-19 16:47:03 +0300

    Support multiple queries in one file (#5)

--- src/codegen.rs
+++ src/codegen.rs
@@ -3,10 +3,10 @@ pub mod sqlalchemy;
 
 use std::error::Error;
 
-use crate::check_query::QueryFn;
+use crate::{check_query::QueryFn, config::FeatureSet};
 
 pub trait CodeGen {
     fn push(&mut self, file_name: &str, query: QueryFn) -> Result<(), Box<dyn Error>>;
 
-    fn finalize(&self) -> Result<String, Box<dyn Error>>;
+    fn finalize(&self, features: &FeatureSet) -> Result<String, Box<dyn Error>>;
 }
