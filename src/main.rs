614630244b621d5e254e8635e5685c5f813e8fea
jnlt3 <dsekercioglu2003@gmail.com>
2025-02-24 17:35:49 +0300

    Deprecate database URL in .toml config

--- src/main.rs
+++ src/main.rs
@@ -10,26 +10,18 @@ use std::error::Error;
 
 use clap::*;
 use commands::{Generate, Initialize};
-use config::{SqlInferConfig, SqlInferOptions};
 
 #[derive(Parser)]
-#[command(name = "sql-infer")]
-#[command(bin_name = "sql-infer")]
+#[command(name = "sql-infer", bin_name = "sql-infer", version = "0.6.2")]
 enum Command {
     Generate(Generate),
     Init(Initialize),
 }
 
-fn get_config() -> Result<SqlInferConfig, Box<dyn Error>> {
-    let content = std::fs::read_to_string("sql-infer.toml")?;
-    let options: SqlInferOptions = toml::from_str(&content)?;
-    Ok(options.into_config())
-}
-
 fn main() -> Result<(), Box<dyn Error>> {
     let command = Command::parse();
     match command {
-        Command::Generate(args) => args.generate(get_config()?),
+        Command::Generate(args) => args.generate(),
         Command::Init(args) => args.init(),
     }
 }
